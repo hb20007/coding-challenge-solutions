9ef89a3f3e109c6734b8bb6d8f89d977fb879f57
Hanna Sababa <hzsababa@outlook.com>
2018-02-18 18:48:11 +0200

    Added 4th solution

--- 4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays.py
@@ -2,13 +2,22 @@
 There are two sorted arrays nums1 and nums2 of size m and n respectively.
 Find the median of the two sorted arrays (together).
 """
+import statistics
 
 
 class Solution:
-    class Solution:
-        def findMedianSortedArrays(self, nums1, nums2):
-            """
-            :type nums1: List[int]
-            :type nums2: List[int]
-            :rtype: float
-            """
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        # SOLUTION 1: USING THE STATISTICS LIBRARY
+        nums1.extend(nums2)
+        return statistics.median(sorted(nums1))
+
+        # SOLUTION 2: WITHOUT THE STATISTICS LIBRARY
+        nums1.extend(nums2)
+        nums1.sort()
+        n = len(nums1)
+        return nums1[n // 2] if n % 2 else sum(nums1[(n // 2 - 1):(n // 2 + 1)]) / 2
---
+++ 5-longest-palindromic-substring.py
@@ -0,0 +1,12 @@
+"""
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+"""
+
+
+class Solution:
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        
